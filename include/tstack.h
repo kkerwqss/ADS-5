b353c79a1addcda38f6d5645958cdf50f307c802
kkerwqss <banykinaks@yandex.ru>
2024-04-08 16:47:19 +0300

    Update

--- include/tstack.h
+++ include/tstack.h
@@ -4,7 +4,44 @@
 
 template<typename T, int size>
 class TStack {
-  // добавьте код стека
+  private:
+    T * data = new T[size];
+    int top = 0;
+  public:
+    TStack () {
+      top = 0;
+    }
+
+    ~TStack() {delete[] data}
+
+    void push(const T& item) {
+      if (top > size - 1) {
+        throw std::string("Full!!");
+      } else {
+        data[top++] = item;
+      }
+    }
+
+    T pop() {
+      if (top == 0) {
+        throw std::string("Empty!!");
+      } else {
+        top--;
+        return data[top];
+      }
+    }
+
+    T stTop() {
+      if (top > 0) {
+        return data[top - 1];
+      } else {
+        return -1;
+      }
+    }
+
+    bool isEmpty() const {
+      return top == 0;
+    }
 };
 
-#endif  // INCLUDE_TSTACK_H_
+#endif
--- src/alg.cpp
+++ src/alg.cpp
@@ -3,12 +3,84 @@
 #include <map>
 #include "tstack.h"
 
+int getPriority(char sym) {
+  if (sym == '(') {
+    return 0;
+  } else if (sym == ')') {
+    return 1;
+  } else if (sym == '+' || sym == '-') {
+    return 2;
+  } else if (sym == '*' || sym == '/') {
+    return 3;
+  } else {
+    return -1;
+  }
+}
+
+int operation(int a, int b, char sym) {
+  switch (sym) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    case '/':
+      return a / b;
+  }
+  return 0;
+}
+
 std::string infx2pstfx(std::string inf) {
-  // добавьте код
-  return std::string("");
+  TStack<char, 100> stack;
+  std::string post;
+  int priority = 0;
+  for (int i = 0; inf[i]; i++) {
+    if (inf[i] >= '0' && inf[i] <= '9') {
+      post += inf[i];
+      if (i != inf.length() - 1) {
+        post += " ";
+      }
+    } else if (inf[i] == '(') {
+      stack.push(inf[i]);
+    } else if (stack.isEmpty()) {
+      stack.push(inf[i]);
+    } else if (inf[i] == ')') {
+      while (stack.stTop() != '(') {
+        post += stack.pop();
+        post += ' ';
+      }
+      stack.pop();
+    } else if (getPriority(inf[i]) <= stack.stTop()) {
+      while (getPriority(inf[i]) <= stack.stTop()) {
+        post += stack.pop();
+      }
+      post += ' ';
+      stack.push(inf[i]);
+    }
+  }
+  while (!stack.isEmpty()) {
+    post += stack.pop();
+    post += ' ';
+  }
+  return post;
 }
 
 int eval(std::string pref) {
-  // добавьте код
-  return 0;
+  int num1 = 0, num2 = 0;
+  TStack<char,100> stack;
+  std::string line;
+  for (int i = 0; pref[i]; i++) {
+    if (pref[i] >= '0' && pref[i] <= '9') {
+        line += pref[i];
+    } else if (pref[i] == ' ') {
+      stack.push(std::stoi(line));
+      line = "";
+    } else {
+      num1 = stack.pop();
+      num2 = stack.pop();
+      stack.push(operation(num1, num2, pref[i]));
+    }
+  }
+  return stack.pop();
 }
