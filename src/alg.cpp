87dfdc058c88c4ad7f4f80877face9cac82b3e47
kkerwqss <banykinaks@yandex.ru>
2024-04-09 18:32:43 +0300

    update

--- src/alg.cpp
+++ src/alg.cpp
@@ -31,6 +31,10 @@ int operation(int a, int b, char sym) {
   return 0;
 }
 
+bool isOperator(char sym) {
+  return sym == '+' || sym == '-' || sym == '*' || sym == '/';
+}
+
 std::string infx2pstfx(std::string inf) {
   TStack<char, 100> stack;
   std::string post;
@@ -38,10 +42,11 @@ std::string infx2pstfx(std::string inf) {
   for (int i = 0; inf[i]; i++) {
     if (inf[i] >= '0' && inf[i] <= '9') {
       post += inf[i];
-      if (i != inf.length() - 1) {
+      if ((i != inf.length() - 1) || (!stack.isEmpty())) {
         post += " ";
       }
-    } else if (inf[i] == '(') {
+    }
+    else if (inf[i] == '(') {
       stack.push(inf[i]);
     } else if (stack.isEmpty()) {
       stack.push(inf[i]);
@@ -51,11 +56,13 @@ std::string infx2pstfx(std::string inf) {
         post += ' ';
       }
       stack.pop();
-    } else if (getPriority(inf[i]) <= stack.stTop()) {
-      while (getPriority(inf[i]) <= stack.stTop()) {
+    } else if (getPriority(inf[i]) <= getPriority(stack.stTop())) {
+      while (getPriority(inf[i]) <= getPriority(stack.stTop())) {
         post += stack.pop();
       }
       post += ' ';
+      stack.push(inf[i]);
+    } else {
       stack.push(inf[i]);
     }
   }
@@ -68,17 +75,17 @@ std::string infx2pstfx(std::string inf) {
 
 int eval(std::string pref) {
   int num1 = 0, num2 = 0;
-  TStack<char, 100> stack;
+  TStack<int, 100> stack;
   std::string line;
   for (int i = 0; pref[i]; i++) {
     if (pref[i] >= '0' && pref[i] <= '9') {
-        line += pref[i];
-    } else if (pref[i] == ' ') {
+      line += pref[i];
+    } else if (pref[i] == ' ' && line.length() > 0) {
       stack.push(std::stoi(line));
       line = "";
-    } else {
-      num1 = stack.pop();
+    } else if (isOperator(pref[i])) {
       num2 = stack.pop();
+      num1 = stack.pop();
       stack.push(operation(num1, num2, pref[i]));
     }
   }
